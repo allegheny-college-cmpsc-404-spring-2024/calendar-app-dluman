c2032ed2cdedd27c5d2bd73b53f33fe403ce8c4e
dluman <dluman@allegheny.edu>
2024-03-18 11:07:47 -0400

    Updating with DB work

--- src/calendar_display.py
+++ src/calendar_display.py
@@ -1,28 +1,50 @@
 import datetime
 import calendar
 
+from calendar_db import DB
+
 class Calendar:
 
-  @staticmethod
-  def months_of_year(month_int: int = 12) -> str:
-    months = {
-      1: "January",
-      2: "February",
-      3: "March",
-      4: "April",
-      5: "May",
-      6: "June",
-      7: "July",
-      8: "August",
-      9: "September",
-      10: "October",
-      11: "November",
-      12: "December"
-    }
-    return months[month_int]
+  def __init__(self, month: int = 12, year: int = 1969):
+    self.month = month
+    self.year = year
+    self.number_of_days = calendar.monthrange(self.year, self.month)[1]
+    self.data = DB().get_appointments_data_only(
+      month = self.months_of_year(month)["short"],
+      year = year
+    )
 
   @staticmethod
-  def days_of_week(weekday: int = 0) -> str:
+  def months_of_year(month: int = 12) -> str:
+    response = {"int": month}
+    months = {
+      1: {"long":"January", "short": "Jan"},
+      2: {"long":"February", "short": "Feb"},
+      3: {"long":"March", "short": "Mar"},
+      4: {"long":"April", "short": "Apr"},
+      5: {"long":"May", "short": "May"},
+      6: {"long":"June", "short": "Jun"},
+      7: {"long":"July", "short": "Jul"},
+      8: {"long":"August", "short": "Aug"},
+      9: {"long":"September", "short": "Sep"},
+      10: {"long":"October", "short": "Oct"},
+      11: {"long":"November", "short": "Nov"},
+      12: {"long":"December", "short": "Dec"}
+    }
+    response.update(months[month])
+    return response
+
+  @staticmethod
+  def parse_date(date: str = "") -> dict:
+    parts = date.split("-")
+    data = {}
+    data["year"] = parts[0]
+    data["month_data"] = Calendar.months_of_year(int(parts[1]))
+    data["month"] = data["month_data"]["short"]
+    data["day"] = parts[2]
+    return data
+
+  def __days_of_week(self) -> str:
     days = {
       0: "Monday",
       1: "Tuesday",
@@ -32,20 +54,35 @@ class Calendar:
       5: "Saturday",
       6: "Sunday"
     }
-    return days[weekday]
+    return days[self.weekday]
 
-  @staticmethod
-  def first_day_of_week(month: int = 12, year: int = 1969) -> int:
-    day = datetime.datetime(year, month, 1)
+  def __update_with_data(self, day):
+    for entry in self.data:
+      if day["date"] == entry["day"]:
+        day["data"] = entry["id"]
+    return day
+
+  def __first_day_of_week(self) -> int:
+    day = datetime.datetime(self.year, self.month, 1)
     return day.weekday()
 
-  @staticmethod
-  def date_display(month: int = 12, year: int = 1969) -> dict:
-    number_of_days = calendar.monthrange(year, month)[1]
-    first_day = Calendar.first_day_of_week(month = month, year = year)
+  def __make_date_array(self) -> list:
+    days = list()
+    first_day = self.__first_day_of_week()
+    if first_day > 0:
+      days = [{"date":''}] * first_day
+    days += [
+      {"date": n + 1} for n in range(self.number_of_days)
+    ]
+    days = list(map(self.__update_with_data,days))
+    return days
+
+  def date_display(self) -> dict:
     return {
-      "month_text": Calendar.months_of_year(month_int = month),
-      "days": number_of_days,
-      "start": first_day,
-      "days_of_week": ['M','T','W','R','F','Sa','Su']
+      "display_month": self.month,
+      "display_year": self.year,
+      "month_text": f"{self.months_of_year(self.month)['long']} {self.year}",
+      "number_of_days": self.number_of_days,
+      "days": self.__make_date_array(),
+      "days_of_week": ['Su','M','T','W','R','F','Sa']
     }
--- src/main.py
+++ src/main.py
@@ -1,19 +1,50 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, Response, redirect, request, render_template
+
+import datetime
+
 from calendar_display import *
+from calendar_db import DB
 
 app: Flask = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
+  month = None
+  year = None
+  try:
+    month = request.form["month"]
+    year = request.form["year"]
+  except:
+    now = datetime.datetime.now()
+    month = now.month
+    year = now.year
+  return render_template(
+    "layout.html",
+    data = {
+      "month": month,
+      "year": year
+    }
+  )
+
+@app.route("/calendar", methods = ["GET"])
+def calendar():
     month, year = (
-      int(request.args.get("month")) or 12,
-      int(request.args.get("year")) or 1969
+      request.args.get("month"),
+      request.args.get("year")
     )
-    print(month, year)
+    calendar = Calendar(int(month), int(year))
     return render_template(
-      "index.html",
-      display_calendar = Calendar.date_display(month, year)
+      "components/calendar.html",
+      display_calendar = calendar.date_display()
     )
+
+@app.route("/modals/add-modal", methods = ["GET"])
+def add_modal_get():
+  return Response(status=200)
+
+@app.route("/events/new", methods = ["POST"])
+def new_event_post():
+    return Response(status=200)
 
 if __name__ == "__main__":
     app.run(port = 5001)
